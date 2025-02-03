a5f25a09460e3a0c8d086b426fc9793b14887a36
Jose Canario <jea.canario@gmail.com>
2025-02-03 21:48:11 +0000

    flashcards now correctly change appearance on selection, mostly done

--- src/App.jsx
+++ src/App.jsx
@@ -13,50 +13,57 @@ const questions = [
   {
     id: 3457,
     question: "What language is React based on?",
-    answer: "JavaScript"
+    answer: "JavaScript",
   },
   {
     id: 7336,
     question: "What are the building blocks of React apps?",
-    answer: "Components"
+    answer: "Components",
   },
   {
     id: 8832,
     question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
+    answer: "JSX",
   },
   {
     id: 1297,
     question: "How to pass data from parent to child components?",
-    answer: "Props"
+    answer: "Props",
   },
   {
     id: 9103,
     question: "How to give components memory?",
-    answer: "useState hook"
+    answer: "useState hook",
   },
   {
     id: 2002,
     question:
       "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
+    answer: "Controlled element",
+  },
 ];
 
 function FlashCards() {
-  return <ul className="flashcards">
-   {questions.map((q)=><FlashCard {...q} key={q.id}/>)}
-  </ul>;
+  return (
+    <ul className="flashcards">
+      {questions.map((q) => (
+        <FlashCard {...q} key={q.id} />
+      ))}
+    </ul>
+  );
 }
 
-function FlashCard(q){
-  const [cardContent, setcardContent] = useState(q.question)
+function FlashCard(q) {
+  const [cardContent, setcardContent] = useState(q.question);
 
-  function handleClick(){
-    cardContent  === q.question ? setcardContent(q.answer) : setcardContent(q.question);
+  function handleClick(e) {
+    if (cardContent === q.question) {
+      e.target.className = "selected";
+      setcardContent(q.answer);
+    } else {
+      setcardContent(q.question);
+      e.target.className = "";
+    }
   }
-
-  return <li onClick={handleClick}>
-    {cardContent}
-  </li>
+  return <li onClick={handleClick}>{cardContent}</li>;
 }
